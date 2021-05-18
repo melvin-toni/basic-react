693db2e4475a6d562af82afc9d6df8ab53d3a082
Melvin Toni Gustave <melvin.working@gmail.com>
2021-05-18 22:49:45 +0800

    login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [firmIdentifier, setFirmIdentifier] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const submitReview = () => {
+    Axios.post('https://dev.innov.id/bara-mcp/public/api/client-login', {
+      firmIdentifier: firmIdentifier,
+      email: email,
+      password: password
+    }).then((res) => {
+      alert('Successfully login');
+      console.log(JSON.stringify(res));
+    }).catch(err => alert(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD APPS</h1>
+
+      <div className="form">
+        <label>Firm ID</label> <input type="text" name="firmIdentifier" onChange={(e) => {setFirmIdentifier(e.target.value)}}/>
+        <label>Email</label> <input type="text" name="email"  onChange={(e) => {setEmail(e.target.value)}}/>
+        <label>Password</label> <input type="password" name="password" onChange={(e) => {setPassword(e.target.value)}} />
+
+        <button onClick={submitReview}>Submit</button>
+      </div>
     </div>
   );
 }
